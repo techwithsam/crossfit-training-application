296d994f456faac37acebdc93eef806ae0f8da76
Samuel Adekunle <samuelbeebest@gmail.com>
2022-09-15 00:22:13 +0100

    Documentation

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -11,6 +11,44 @@ const cache = apicache.middleware;
 
 // router.post("/", authenticate, authorize, workoutController.createNewWorkout);
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *        5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/", workoutController.getAllWorkouts);
